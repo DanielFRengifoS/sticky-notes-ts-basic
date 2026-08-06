2ecfdca102bd678fb99063831356d21a74c3ff26
DrengifoIndeed <drengifo@contractor.indeed.com>
2026-08-06 11:44:17 -0500

    DART-001: punchier line

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <div className="app">
       <header className="app__header">
         <h1>Sticky Notes</h1>
-        <p>A desktop sticky-notes board.</p>
+        <p>Jot it, drag it, make it stick.</p>
       </header>
 
       <Board />
